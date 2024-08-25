a58028707080877ea96c3b589eef2b1328fe780d
jdthgp27 <147777311+jdthgp27@users.noreply.github.com>
2024-08-25 18:49:38 +0200

    Add files via upload

---
+++ userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { createUser, getUsers } = require('../controllers/userController');
+const router = express.Router();
+
+// Ruta para crear un usuario
+router.post('/', createUser);
+
+// Ruta para obtener todos los usuarios
+router.get('/', getUsers);
+
+module.exports = router;
